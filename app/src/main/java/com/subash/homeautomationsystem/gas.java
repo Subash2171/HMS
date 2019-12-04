92c259e40975fa5fefbebd4919a70a206de9ecac
Subash2171 <subashgurung217@gmail.com>
2019-12-04 15:13:51 +0545

    sign up completed with progress bar + water, temp

---
+++ app/src/main/java/com/subash/homeautomationsystem/gas.java
@@ -0,0 +1,14 @@
+package com.subash.homeautomationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class gas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gas);
+    }
+}
---
+++ app/src/main/java/com/subash/homeautomationsystem/light.java
@@ -0,0 +1,14 @@
+package com.subash.homeautomationsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class light extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_light);
+    }
+}
